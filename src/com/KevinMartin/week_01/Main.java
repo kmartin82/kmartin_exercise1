8cb83ba557c9ea334ce1453d5e1f2fe42a4042bb
Kevin Martin <kmartin82@student.cscc.edu>
2016-08-30 20:26:57 -0400

    Initial commit

---
+++ src/com/KevinMartin/week_01/Main.java
@@ -0,0 +1,15 @@
+package com.KevinMartin.week_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String cityName = "columbus";
+        int zipCode = 43215;
+        float[] temperature = {32,25,27,40,45};
+        float averageTemp = (temperature[0]+temperature[1]+temperature[2]+temperature[3]+temperature[4])/5;
+        System.out.println("City " + cityName);
+        System.out.println("Zip Code " + zipCode);
+        System.out.println("Average Temperature " + averageTemp);
+
+    }
+}
